f2734801cf27e58c8699b82e7ba1aa4825638643
lasha <lashatatu@gmail.com>
2023-10-22 17:30:05 +0400

    218

--- src/Components/CityItem.jsx
+++ src/Components/CityItem.jsx
@@ -13,10 +13,11 @@ const CityItem = ({city}) => {
     emoji,
     date,
     id,
+    position,
   } = city;
   return (
     <li>
-      <Link className={styles.cityItem} to={`${id}`}>
+      <Link className={styles.cityItem} to={`${id}?lat=${position.lat}&lng=${position.lng}`}>
         <span className={styles.emoji}>{emoji}</span>
         <h3 className={styles.name}>{cityName}</h3>
         <time className={styles.date}>{formatDate(date)}</time>
--- src/Components/Map.jsx
+++ src/Components/Map.jsx
@@ -1,9 +1,21 @@
 import style from './Map.module.css';
+import { useSearchParams } from 'react-router-dom';
 
 export default function Map () {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lat = searchParams.get('lat');
+  const lng = searchParams.get('lng');
   return (
     <div className={style.mapContainer}>
-      map
+      <h1>Map</h1>
+      <h1>Position: {lat}, {lng}</h1>
+      <button
+        onClick={() => {
+          setSearchParams({
+            lat: 23,
+            lng: 50,
+          });
+        }}>change pos</button>
     </div>
   );
 }
